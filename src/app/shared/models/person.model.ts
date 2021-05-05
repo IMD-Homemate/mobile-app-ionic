6b83c4e7097105cde40bf809973ff5b9fc05bf6c
Vanessa Booyse <booyse.vanessa@gmail.com>
2021-05-05 23:19:22 +0200

    Update person.model.ts

--- src/app/shared/models/person.model.ts
+++ src/app/shared/models/person.model.ts
@@ -1,2 +1,14 @@
 export class Person {
+    id: number;
+    firstname : string;
+    lastname :  string;
+    birthdate : number;
+    gender : Gender;
+    photo : string;
+
 }
+
+enum Gender {
+    male = "M",
+    female = "F"
+  }
