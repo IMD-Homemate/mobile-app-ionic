1b5b719cf98736302f4315d742a59b03e8a7f076
barthuygen <bart.huygen@me.com>
2021-06-10 16:40:08 +0200

    added some preferences stuff

--- src/app/preferences-edit/preferences-edit.page.ts
+++ src/app/preferences-edit/preferences-edit.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../model/authentication-service';
+import { PersonService, PreferencesService } from '../model/crud.service';
+import { Preferences } from '../shared/models/preferences.model';
 
 @Component({
   selector: 'app-preferences-edit',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreferencesEditPage implements OnInit {
 
-  constructor() { }
+  firstname: string;
+  lastname:string;
+  preferences: any;
+
+  constructor(private personService: PersonService, private authService: AuthenticationService, private preferenceService: PreferencesService) { }
 
   ngOnInit() {
+    this.personService.getPerson(this.authService.uuid).subscribe((data) => {
+      this.firstname = data['firstname'];
+      this.lastname = data['lastname'];
+    });
+    this.preferenceService.getPerson(this.authService.uuid).subscribe((data) => {
+      this.preferences = data;
+      console.log(this.preferences);
+    });
   }
 
 }
