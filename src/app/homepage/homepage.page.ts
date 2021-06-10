869bef963dc175bdb18d7560e5f68047144b1b6a
Vanessa Booyse <booyse.vanessa@gmail.com>
2021-06-10 13:16:16 +0200

    Personen ophalen vanuit homepage

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../model/authentication-service';
+import { ProfileImageService, PersonService } from './../model/crud.service';
+
+export class Person {
+  $key: string;
+  firstname: string;
+  lastname: string;
+  birthdate: string;
+  gender: string;
+  photo: string;
+}
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +19,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagePage implements OnInit {
 
-  constructor() { }
+  url: any;
+  persons: Person[];
+
+  constructor(private authService :AuthenticationService, private pImageService : ProfileImageService, private personService: PersonService, private crudService: PersonService, private router: Router) { }
 
   ngOnInit() {
+
+    this.crudService.getPersons().subscribe((res) => {
+      this.persons = res.map((t) => {
+        return {
+          id: t.payload.doc.id,
+          ...t.payload.doc.data() as Person
+        };
+      })
+    });
+  }
+
+  next(id){
+    this.router.navigate(['chat'], { state: {id: id} });
+  }
+
+  todoList() {
+    this.crudService.getPersons()
+    .subscribe((data) => {
+      console.log(data)
+    })
   }
 
 }
